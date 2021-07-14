0776444b3dd16655efc56286defbe4a4a5d0f5aa
Cameron Weston <cameronbweston@gmail.com>
2021-07-13 20:51:08 -0500

    Add data folder with db, add skillsview folder and index.ejs for skills

---
+++ data/skills-db.js
@@ -0,0 +1,10 @@
+export { 
+
+}
+
+const skills = [
+  {skill: 'JavaScript wizard', _id: 125223},
+  {skill: 'EJS amateur', _id: 127904},
+  {skill: 'Google hero', _id: 123456},
+  {skill: 'Cry on command', _id: 139608},
+]
